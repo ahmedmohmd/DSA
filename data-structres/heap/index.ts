19f7844e4099f5c707cf3965fd088fafca4329eb
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2023-01-07 06:55:05 +0200

    initial commit

---
+++ data-structres/heap/index.ts
@@ -0,0 +1,113 @@
+class Heap {
+  data: number[];
+  size: number;
+
+  constructor() {
+    this.data = [];
+    this.size = 0;
+  }
+
+  insert(value: number) {
+    this.data.push(value);
+    this.size++;
+
+    this.bubbleUp();
+  }
+
+  parent(index) {
+    let parentIndex = Math.floor((index - 1) / 2);
+
+    return parentIndex;
+  }
+
+  bubbleUp() {
+    let index = this.size - 1;
+
+    while (index > 0 && this.data[index] > this.data[this.parent(index)]) {
+      [this.data[index], this.data[this.parent(index)]] = [
+        this.data[this.parent(index)],
+        this.data[index],
+      ];
+
+      index = this.parent(index);
+    }
+  }
+
+  public remove() {
+    if (this.isEmpty) {
+      throw new Error("The Heap is Empty!");
+    }
+
+    this.data.shift();
+    this.size--;
+
+    let index = 0;
+
+    while (index <= this.size && !this.isValidParent(index)) {
+      let largerChildIndex = this.getLargerIndex(index);
+
+      [this.data[index], this.data[largerChildIndex]] = [
+        this.data[largerChildIndex],
+        this.data[index],
+      ];
+
+      index = largerChildIndex;
+    }
+
+    this.data = this.data.filter((item) => item);
+  }
+
+  //    Accecoures
+  private leftChildIndex(index) {
+    return index * 2 + 1;
+  }
+
+  private rightChildIndex(index) {
+    return index * 2 + 2;
+  }
+
+  private leftChild(index) {
+    return this.data[this.leftChildIndex(index)];
+  }
+
+  private rightChild(index) {
+    return this.data[this.rightChildIndex(index)];
+  }
+
+  private isValidParent(index) {
+    if (!this.leftChild(index)) {
+      return true;
+    }
+
+    if (!this.rightChild(index)) {
+      return this.data[index] < this.leftChild(index);
+    }
+
+    if (
+      this.data[index] < this.leftChild(index) &&
+      this.data[index] < this.rightChild(index)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private getLargerIndex(index) {
+    if (!this.leftChild(index)) {
+      return index;
+    }
+
+    if (!this.rightChild(index)) {
+      return this.leftChildIndex(index);
+    }
+
+    return this.leftChild(index) > this.rightChild(index)
+      ? this.leftChildIndex(index)
+      : this.rightChildIndex(index);
+  }
+
+  private get isEmpty() {
+    return this.size === 0;
+  }
+}
