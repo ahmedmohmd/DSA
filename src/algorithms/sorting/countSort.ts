ec497c5e7d79961968e94558b19ef7ae12fcf537
Ahmed Mohamed <Ahmed Mohamed>
2023-01-15 18:21:48 +0200

    finish count-sort algorithm

---
+++ src/algorithms/sorting/countSort.ts
@@ -0,0 +1,26 @@
+class CountSort {
+  public sort(array: number[], max?: number): void | [] {
+    if (array.length === 0) return [];
+
+    const maxNum: number = (max || this.getMax(array)) + 1;
+    const counts: number[] = new Array(maxNum).fill(0);
+
+    for (let item of array) {
+      counts[item]++;
+    }
+
+    let k: number = 0;
+    for (let i: number = 0; i < counts.length; i++) {
+      for (let j: number = 0; j < counts[i]; j++) {
+        array[k] = i;
+        k++;
+      }
+    }
+  }
+
+  private getMax(array: number[]): number {
+    return Math.max(...array);
+  }
+}
+
+export default CountSort;
