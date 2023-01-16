9395f03ebc431d45e2537f5f93b8b0c070e2c92c
Ahmed Mohamed <Ahmed Mohamed>
2023-01-16 08:18:05 +0200

    finish merge-sort algorithm

---
+++ src/algorithms/sorting/mergeSort.ts
@@ -0,0 +1,31 @@
+class MergeSort {
+  public sort(array: number[]): number[] {
+    if (array.length === 1) return array;
+
+    const middle: number = Math.floor(array.length / 2);
+    const left: number[] = array.slice(0, middle);
+    const right: number[] = array.slice(middle);
+
+    return this.merge(this.sort(left), this.sort(right));
+  }
+
+  private merge(left: number[], right: number[]): number[] {
+    const result: number[] = [];
+    let leftIndex: number = 0;
+    let rightIndex: number = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  }
+}
+
+export default MergeSort;
