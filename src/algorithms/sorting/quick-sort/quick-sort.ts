6d2978372dfc9db0b36acc51d2e420b67680850d
ahmedmohmd <er.ahmed.3301@gmail.com>
2023-05-08 18:24:09 +0300

    algorithms/refactor quick sort algorithm

--- src/algorithms/sorting/quick-sort/quick-sort.ts
+++ src/algorithms/sorting/quick-sort/quick-sort.ts
@@ -1,32 +1,36 @@
 class QuickSort {
-  public sort(
+  public static sort(
     array: number[],
     start: number = 0,
     end: number = array.length - 1
   ): void {
     if (start >= end) return;
 
-    let boundary = this.partition(array, start, end);
+    let boundary = QuickSort.partition(array, start, end);
 
-    this.sort(array, start, boundary - 1);
-    this.sort(array, boundary + 1, end);
+    QuickSort.sort(array, start, boundary - 1);
+    QuickSort.sort(array, boundary + 1, end);
   }
 
-  private partition(array: number[], start: number, end: number): number {
+  private static partition(
+    array: number[],
+    start: number,
+    end: number
+  ): number {
     let pivot = array[end];
     let boundary = start - 1;
 
     for (let i = start; i <= end; i++) {
       if (array[i] <= pivot) {
         boundary++;
-        this.swap(array, i, boundary);
+        QuickSort.swap(array, i, boundary);
       }
     }
 
     return boundary;
   }
 
-  private swap(array: any[], first: number, second: number) {
+  private static swap(array: any[], first: number, second: number) {
     [array[first], array[second]] = [array[second], array[first]];
   }
 }
