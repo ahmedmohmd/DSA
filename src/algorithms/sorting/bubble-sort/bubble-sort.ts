3f451a0df6b63280122d4c44ce7db51c0fec7b85
ahmedmohmd <er.ahmed.3301@gmail.com>
2024-02-27 22:03:01 +0200

    Refactor and imporove bubble-sort algorithm performance

--- src/algorithms/sorting/bubble-sort/bubble-sort.ts
+++ src/algorithms/sorting/bubble-sort/bubble-sort.ts
@@ -1,14 +1,20 @@
 class BubbleSort {
-  static sort(array: number[]): number[] {
+  static sort(array: number[]): void {
+    let isSorted: boolean;
+
     for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < array.length; j++) {
-        if (array[j + 1] < array[j]) {
-          [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      isSorted = true;
+
+      for (let j = 0; j < array.length - i; j++) {
+        if (array[j] < array[j - 1]) {
+          [array[j], array[j - 1]] = [array[j - 1], array[j]];
+
+          isSorted = false;
         }
       }
-    }
 
-    return array;
+      if (isSorted) return;
+    }
   }
 }
 
